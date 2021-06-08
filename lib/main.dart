0bd5c59b8c829a6beafe9f0f4ae2a748399cbbe9
hyperinactive <hyperinactive.box@gmail.com>
2021-06-09 01:20:23 +0200

    scrolling and listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -27,22 +27,27 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Expenses'),
       ),
-      body: Column(
-        // ROW/COLUMN Main/Cross Axis
-        crossAxisAlignment: CrossAxisAlignment.start,
-        // <Widget> good practive to label the lists
-        children: <Widget>[
-          Card(
-            // Cards are only as big as they need to fit the child
-            child: Container(
-              child: Text('Chart placeholder'),
-              width: double.infinity,
-              color: Colors.blue,
+      // scroll view gives us scroll functionality
+      // to avoid overflows when keyboard pushes the page up when it opens
+      // if added on a child widget, it is important to give it height or the scroll won't work properly
+      body: SingleChildScrollView(
+        child: Column(
+          // ROW/COLUMN Main/Cross Axis
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // <Widget> good practive to label the lists
+          children: <Widget>[
+            Card(
+              // Cards are only as big as they need to fit the child
+              child: Container(
+                child: Text('Chart placeholder'),
+                width: double.infinity,
+                color: Colors.blue,
+              ),
+              elevation: 5, // drop shadow size
             ),
-            elevation: 5, // drop shadow size
-          ),
-          UserTransaction(),
-        ],
+            UserTransaction(),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -9,51 +9,66 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // render card widgets using the transaction list
-      children: transactions.map((e) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                child: Text(
-                  // js string literals
-                  '\$${e.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.pink,
-                  ),
-                ),
-                // margin and padding, both use EdgeInsets class
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.pink, width: 2)),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    e.title,
+    return Container(
+      height: 300,
+      // Column + SingleChildScrollView == ListView widget
+      // still needs height though
+
+      // ListView if provided with children, will render them all!!!
+      // use the builder() constructor instead, as it will only render visible widgets
+
+      // builder will call for rendering each time it's needed to render a widget
+      // it needs an itemBuilder, a function given by flutter, gives a context and index
+      // index tells the currently rendered item
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    // js string literals
+                    '\$${transactions[index].amount}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                      fontSize: 20,
+                      color: Colors.pink,
                     ),
                   ),
-                  Text(
-                    // from intl package
-                    DateFormat.yMEd().add_jm().format(e.date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                  // margin and padding, both use EdgeInsets class
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.pink, width: 2)),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                    Text(
+                      // from intl package
+                      DateFormat.yMEd()
+                          .add_jm()
+                          .format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        // render card widgets using the transaction list
+      ),
     );
   }
 }
